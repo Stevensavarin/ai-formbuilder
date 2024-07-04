7614998f00d616245639b302fe6cb9c8d653856a
steven_savarin <srssdesing@gmail.com>
2024-07-05 01:34:48 +0200

    Auth

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,61 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+<section className="bg-white">
+  <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+    <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+      <img
+        alt=""
+        src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGZ1Ym5tb3UyMmloOHg0eTBpeTNkYnNkOTViajhrdXh3N2tjMTJiayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FYDgDc6Xi1o3x02hrQ/giphy.webp"
+        className="absolute inset-0 h-full w-full object-cover opacity-80"
+      />
+
+      <div className="hidden lg:relative lg:block lg:p-12">
+        <a className="block text-white" href="#">
+          <span className="sr-only">Home</span>
+          <svg id="logo-39" width="50" height="40" viewBox="0 0 50 40" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M25.0001 0L50 15.0098V24.9863L25.0001 40L0 24.9863V15.0099L25.0001 0Z" fill="#A5B4FC" class="ccompli2"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M0 15.0098L25 0L50 15.0098V24.9863L25 40L0 24.9863V15.0098ZM25 33.631L44.6967 21.8022V18.1951L44.6957 18.1945L25 30.0197L5.30426 18.1945L5.3033 18.1951V21.8022L25 33.631ZM25 24.5046L40.1018 15.4376L36.4229 13.2298L25 20.0881L13.5771 13.2298L9.89822 15.4376L25 24.5046ZM25 14.573L31.829 10.4729L25 6.37467L18.171 10.4729L25 14.573Z" fill="#4F46E5" class="ccustom"></path> <path d="M25.0001 0L0 15.0099V24.9863L25 40L25.0001 0Z" fill="#A5B4FC" class="ccompli2" fill-opacity="0.3"></path> </svg>
+        </a>
+
+        <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+          Welcome to AI Form Builder
+        </h2>
+
+        <p className="mt-4 leading-relaxed text-white/90">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+          quibusdam aperiam voluptatum.
+        </p>
+      </div>
+    </section>
+
+    <main
+      className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+    >
+      <div className="max-w-xl lg:max-w-3xl">
+        <div className="relative -mt-16 block lg:hidden">
+          <a
+            className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+            href="#"
+          >
+            <span className="sr-only">Home</span>
+            <svg id="logo-39" width="50" height="40" viewBox="0 0 50 40" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M25.0001 0L50 15.0098V24.9863L25.0001 40L0 24.9863V15.0099L25.0001 0Z" fill="#A5B4FC" class="ccompli2"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M0 15.0098L25 0L50 15.0098V24.9863L25 40L0 24.9863V15.0098ZM25 33.631L44.6967 21.8022V18.1951L44.6957 18.1945L25 30.0197L5.30426 18.1945L5.3033 18.1951V21.8022L25 33.631ZM25 24.5046L40.1018 15.4376L36.4229 13.2298L25 20.0881L13.5771 13.2298L9.89822 15.4376L25 24.5046ZM25 14.573L31.829 10.4729L25 6.37467L18.171 10.4729L25 14.573Z" fill="#4F46E5" class="ccustom"></path> <path d="M25.0001 0L0 15.0099V24.9863L25 40L25.0001 0Z" fill="#A5B4FC" class="ccompli2" fill-opacity="0.3"></path> </svg>          </a>
+
+          <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+          Welcome to AI Form Builder
+          </h1>
+
+          <p className="mt-4 leading-relaxed text-gray-500">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+            quibusdam aperiam voluptatum.
+          </p>
+        </div>
+
+        <SignIn path="/sign-in" />
+      </div>
+    </main>
+  </div>
+</section>
+  )
+  
+
+}
--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
 import { ClerkProvider, SignIn } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner";
+//import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         <Header/>
        
         {children}
-        <Toaster />
+        {/* <Toaster /> */}
       </body>
     </html>
     </ClerkProvider>
